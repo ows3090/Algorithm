5930b8615f39fd7084912fc93ec1bd3ef6934363
ows3090 <ows3090@naver.com>
2022-03-23 18:59:49 +0900

    kotlin(baekjoon 4179) : Fire

--- kotlin/README.md
+++ kotlin/README.md
@@ -15,6 +15,7 @@
 - [1926.picture](https://www.acmicpc.net/problem/1926)
 - [2178.mazeexploration](https://www.acmicpc.net/problem/2178)
 - [7576.tomato](https://www.acmicpc.net/problem/7576)
+- [4179.fire](https://www.acmicpc.net/problem/4179)
 <br>
 
 ## Stack
--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -7,61 +7,81 @@ fun inside(n: Int, m: Int, y: Int, x: Int): Boolean {
 
 fun main() = with(System.`in`.bufferedReader()) {
     val input = readLine().split(" ")
-    val m = input.first().toInt()
-    val n = input.last().toInt()
+    val n = input.first().toInt()
+    val m = input.last().toInt()
 
     val arr = Array(n, { Array(m, { 0 }) })
-    val visit = Array(n, { Array(m, { Int.MAX_VALUE }) })
+    val fvisit = Array(n, { Array(m, { Int.MAX_VALUE }) })
+    val jvisit = Array(n, { Array(m, { -1 }) })
     val dir = arrayOf(arrayOf(0, 1), arrayOf(1, 0), arrayOf(0, -1), arrayOf(-1, 0))
 
-    for (i in 0 until n) {
-        readLine().split(" ").forEachIndexed { j, num ->
-            arr[i][j] = num.toInt()
-        }
-    }
+    val jq = LinkedList<Pair<Int,Int>>()
+    val fq = LinkedList<Pair<Int,Int>>()
 
-    val q = LinkedList<Pair<Int,Int>>()
-    for(i in 0 until n){
-        for(j in 0 until m){
-            if(arr[i][j] == 1){
-                q.add(Pair(i,j))
-                visit[i][j] = 0
+    for (i in 0 until n) {
+        readLine().toCharArray().forEachIndexed { j, ch ->
+            when(ch){
+                '#' -> arr[i][j] = -1
+                '.' -> arr[i][j] = 0
+                'J' -> {
+                    arr[i][j] = 1
+                    jq.add(Pair(i,j))
+                    jvisit[i][j] = 0
+                }
+                'F' ->{
+                    arr[i][j] = 2
+                    fq.add(Pair(i,j))
+                    fvisit[i][j] = 0
+                }
             }
         }
     }
 
-    while(!q.isEmpty()){
-        val y = q.peek().first
-        val x = q.peek().second
-        q.poll()
+    while(!fq.isEmpty()){
+        val y = fq.peek().first
+        val x = fq.peek().second
+        fq.poll()
 
         for(d in dir){
             val dy = y + d[0]
             val dx = x + d[1]
 
             if(inside(n,m,dy,dx) && arr[dy][dx] == 0){
-                if(visit[y][x] + 1 < visit[dy][dx]){
-                    q.add(Pair(dy,dx))
-                    visit[dy][dx] = visit[y][x] + 1
-                }
+                if(fvisit[y][x] + 1 < fvisit[dy][dx])
+                fq.add(Pair(dy,dx))
+                fvisit[dy][dx] = fvisit[y][x] + 1
             }
         }
     }
 
     var result = -1
-    loop@for(i in 0 until n){
-        for(j in 0 until m){
-            if(visit[i][j] == Int.MAX_VALUE){
-                if(arr[i][j] == 0){
-                    result = -1
-                    break@loop
-                }
-                continue
+    loop@while(!jq.isEmpty()){
+        val y = jq.peek().first
+        val x = jq.peek().second
+        jq.poll()
+
+        for(d in dir){
+            val dy = y + d[0]
+            val dx = x + d[1]
+
+            if(!inside(n,m,dy,dx)){
+                result = jvisit[y][x] + 1
+                break@loop
             }
-            result = max(result, visit[i][j])
+
+            if(arr[dy][dx] == 0 && jvisit[dy][dx] == -1){
+                if(jvisit[y][x] + 1 <fvisit[y][x]){
+                    jq.add(Pair(dy,dx))
+                    jvisit[dy][dx] = jvisit[y][x] + 1
+                }
+            }
         }
     }
-    println(result)
+    if(result == -1){
+        println("IMPOSSIBLE")
+    }else{
+        println(result)
+    }
 }
 
 
