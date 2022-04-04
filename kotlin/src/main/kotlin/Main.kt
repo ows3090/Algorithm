2c3db986f4dcc61bac6819e697c09d4f6ed2faf8
ows3090 <ows3090@naver.com>
2022-04-04 23:06:12 +0900

    kotlin(programmres) : Lv2.BestAlbum Other Solution commit

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -3,9 +3,7 @@ import kotlin.collections.LinkedHashMap
 
 fun main() = with(System.`in`.bufferedReader()) {
 
-    var arr = mutableListOf<Int>()
-    arr.count { it >= 100 }
-
-    val map = mutableMapOf<String,Int>().map { it }.sortedBy { it.value }
-
+    val arr = arrayOf("pop","R&B","jazz")
+    val grouping = arr.indices.groupBy { arr.get(it) }.toList()
+    println(grouping)
 }
--- kotlin/src/main/kotlin/programmers/hash/BestAlbum.kt
+++ kotlin/src/main/kotlin/programmers/hash/BestAlbum.kt
@@ -33,4 +33,18 @@ class BestAlbum{
 
         return answer.toIntArray()
     }
+
+    /**
+     * Array.indices : Index List 반환
+     * groupBy : 장르가 key가 되어 해당 key들로 그룹핑, 반환 값은 Map<Key, List<Value>>
+     * flatten : 리스트 내 리스트에서 사용되는 함수로 모든 리스트의 원소를 더하는 메소드
+     */
+    fun solution2(genres: Array<String>, plays: IntArray): IntArray {
+        return genres.indices.groupBy { genres[it] }
+                .toList()
+                .sortedByDescending { it.second.sumBy { plays[it] } }
+                .map { it.second.sortedByDescending { plays[it] }.take(2) }
+                .flatten()
+                .toIntArray()
+    }
 }
