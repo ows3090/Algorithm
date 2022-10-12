19eb88ef1ae73ea71708f26b306876b60fc0a149
ows3090 <ows3090@naver.com>
2022-10-12 23:12:13 +0900

    kotlin(programmers) : Lv2.primenumber

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -3,9 +3,11 @@ import kotlin.math.max
 import kotlin.math.sqrt
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val list = listOf(listOf("a","b"), listOf("b","b"), listOf("c","b"), listOf("c","c"), listOf("d","c"))
-    val map = list.groupBy { it.last() }.map { it.value }?.let {
-        if(it.size == 1) it.size else it.size + it.fold(1){acc, list -> acc*list.size }
+    val list = intArrayOf(1,2,3,4,5)
+
+    val q = LinkedList<Pair<Int,Int>>().apply {
+        list.forEachIndexed { index, i -> add(Pair(index, i)) }
     }
-    println(map)
+
+
 }
