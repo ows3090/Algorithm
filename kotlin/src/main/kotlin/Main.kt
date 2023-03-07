94b359d330d9f91f248382424c04fc53d7c1658e
ows3090 <ows3090@naver.com>
2023-03-08 00:24:58 +0900

    Update .gitignore

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,10 +1,3 @@
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
-import kotlin.math.ceil
-import kotlin.math.max
-import kotlin.math.sqrt
-
 fun main() = with(System.`in`.bufferedReader()) {
-
+    println("Hello World")
 }
