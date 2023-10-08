27da9d143926982db86f9faa6bb5510c3da094f2
ows3090 <ows3090@naver.com>
2023-10-08 18:00:41 +0900

    [Array](programmers) : Lv0.CompareArrays

---
+++ kotlin/src/main/kotlin/programmers/array/CompareArrays.kt
@@ -0,0 +1,14 @@
+package programmers.array
+
+class CompareArrays {
+    fun solution(arr1: IntArray, arr2: IntArray): Int =
+        if(arr1.size > arr2.size){
+            1
+        }else if(arr1.size < arr2.size){
+            -1
+        }else {
+            if(arr1.sum() > arr2.sum()) 1
+            else if(arr1.sum() < arr2.sum()) -1
+            else 0
+        }
+}
--- kotlin/src/main/kotlin/programmers/array/README.md
+++ kotlin/src/main/kotlin/programmers/array/README.md
@@ -1,3 +1,4 @@
 ## Array
 - [Lv0.CutArray](https://school.programmers.co.kr/learn/courses/30/lessons/120913)
 - [Lv0.CalculateDependOnArraySize](https://school.programmers.co.kr/learn/courses/30/lessons/181854)
+- [Lv0.CompareArrays](https://school.programmers.co.kr/learn/courses/30/lessons/181856)
