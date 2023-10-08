ac3074cfe36da56af1b6ec0b54d63f37b86da7c4
ows3090 <ows3090@naver.com>
2023-10-08 21:36:17 +0900

    [Basic](programmers) : Lv0.CalculateNumberOfString

---
+++ kotlin/src/main/kotlin/programmers/basic/CalculateNumberOfString.kt
@@ -0,0 +1,13 @@
+package programmers.basic
+
+class CalculateNumberOfString {
+    fun solution(myString: String, pat: String): Int {
+        var answer: Int = 0
+        (0..(myString.length - pat.length)).forEach {
+            if(myString.substring(it, it+pat.length) == pat) {
+                answer++
+            }
+        }
+        return answer
+    }
+}
